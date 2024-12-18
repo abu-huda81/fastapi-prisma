4f7279f73246b5dbfc259aaeb2b41af30f488520
abu-huda81 <abuhuda51020@gmail.com>
2024-12-18 14:49:12 +0300

    hashing password

--- app/services/user_services.py
+++ app/services/user_services.py
@@ -1,6 +1,10 @@
 from typing import List
+
+from fastapi import HTTPException
+from security import get_password_hash
 from schemas.user_schema import User as userSchema, UserCreate, UserUpdate
 from config.prsma_config import prisma
+from prisma.errors import UniqueViolationError
 
 
 class UserService:
@@ -25,13 +29,31 @@ class UserService:
         return userSchema(**user.model_dump())
 
     #  create a new user
+
+
     async def create_user(self, user: UserCreate) -> userSchema:
-        user = await prisma.user.create(data=user.model_dump())
-        return userSchema(**user.model_dump())
+        try:
+            user = await prisma.user.create(data=user.model_dump())
+            # hash the password
+            user.password = get_password_hash(user.password)
+            await prisma.user.update(
+                where={"id": user.id}, data={"password": user.password}
+            )
+            return userSchema(**user.model_dump())
+        except UniqueViolationError as e:
+            if e.code == "P2002":  # Unique constraint failed
+                raise HTTPException(status_code=400, detail="Email already taken")
+            raise
 
     # update a user
     async def update_user(self, id: int, user: UserUpdate) -> userSchema:
         user = await prisma.user.update(where={"id": id}, data=user.model_dump())
+        # hash the password
+        user.password = get_password_hash(user.password)
+        await prisma.user.update(
+            where={"id": user.id}, data={"password": user.password}
+        )
+
         return userSchema(**user.model_dump())
 
     # delete a user
